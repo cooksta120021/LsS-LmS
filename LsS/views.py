a8e735957a0d6936fcc05cadb09cff96253e5401
John <cooksta120021@gmail.com>
2021-10-31 07:59:21 -0500

    3333

--- LsS/views.py
+++ LsS/views.py
@@ -42,14 +42,14 @@ def signup_view(request):
     return render(request, 'register.html', {'form': form})
 
 def login_view(request):
-    if request.method == "GET":
-        form = LoginForm(request.GET)
+    if request.method == "POST":
+        form = LoginForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
             user = authenticate(request, username=data['username'], password=data['password'])
             if user:
                 login(request, user)
-                return HttpResponseRedirect(request,'home.html')
+                return HttpResponseRedirect(request,'/')
     form = LoginForm()
     return render(request, "login.html", {"form": form})
 
