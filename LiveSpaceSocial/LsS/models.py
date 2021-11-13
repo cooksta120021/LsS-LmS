a2d2aae1f1dddd18cb9fdb572a6bf1146b102995
cooksta120021 <cooksta120021@gmail.com>
2021-11-12 20:39:48 -0600

    main update

--- LiveSpaceSocial/LsS/models.py
+++ LiveSpaceSocial/LsS/models.py
@@ -7,6 +7,7 @@ class NewUser(AbstractUser):
     user = models.CharField(max_length=15, unique=True)
     image = models.ImageField(null=True, blank=True)
     follow = models.ManyToManyField('self', symmetrical=False)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.username
